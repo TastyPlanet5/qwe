0453a6baac4984fde7e54c329bf4b0c48e915d35
TastyPlanet5 <114752872+TastyPlanet5@users.noreply.github.com>
2022-09-30 17:05:12 +0300

    Add files via upload

---
+++ BusinessCalc/Form1.Designer.cs
@@ -0,0 +1,285 @@
+﻿namespace BusinessCalc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.addIncomeBTN = new System.Windows.Forms.Button();
+            this.ALL1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.ALL2 = new System.Windows.Forms.Label();
+            this.addExpenseBTN = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.numericUpDown1);
+            this.panel1.Controls.Add(this.dataGridView1);
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Controls.Add(this.ALL1);
+            this.panel1.Controls.Add(this.addIncomeBTN);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(13, 13);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(350, 291);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(-1, -1);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(350, 30);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Incomes";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // addIncomeBTN
+            // 
+            this.addIncomeBTN.Location = new System.Drawing.Point(270, 260);
+            this.addIncomeBTN.Name = "addIncomeBTN";
+            this.addIncomeBTN.Size = new System.Drawing.Size(75, 23);
+            this.addIncomeBTN.TabIndex = 2;
+            this.addIncomeBTN.Text = "Add";
+            this.addIncomeBTN.UseVisualStyleBackColor = true;
+            this.addIncomeBTN.Click += new System.EventHandler(this.addIncomeBTN_Click);
+            // 
+            // ALL1
+            // 
+            this.ALL1.AutoSize = true;
+            this.ALL1.Location = new System.Drawing.Point(3, 196);
+            this.ALL1.Name = "ALL1";
+            this.ALL1.Size = new System.Drawing.Size(36, 13);
+            this.ALL1.TabIndex = 3;
+            this.ALL1.Text = "All: 0$";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Salary",
+            "Gift"});
+            this.comboBox1.Location = new System.Drawing.Point(6, 262);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(171, 21);
+            this.comboBox1.TabIndex = 5;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.numericUpDown2);
+            this.panel2.Controls.Add(this.dataGridView2);
+            this.panel2.Controls.Add(this.comboBox2);
+            this.panel2.Controls.Add(this.ALL2);
+            this.panel2.Controls.Add(this.addExpenseBTN);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Location = new System.Drawing.Point(384, 13);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(350, 291);
+            this.panel2.TabIndex = 7;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Food",
+            "Clothes",
+            "Entertainments"});
+            this.comboBox2.Location = new System.Drawing.Point(6, 262);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(171, 21);
+            this.comboBox2.TabIndex = 5;
+            // 
+            // ALL2
+            // 
+            this.ALL2.AutoSize = true;
+            this.ALL2.Location = new System.Drawing.Point(3, 196);
+            this.ALL2.Name = "ALL2";
+            this.ALL2.Size = new System.Drawing.Size(36, 13);
+            this.ALL2.TabIndex = 3;
+            this.ALL2.Text = "All: 0$";
+            // 
+            // addExpenseBTN
+            // 
+            this.addExpenseBTN.Location = new System.Drawing.Point(270, 260);
+            this.addExpenseBTN.Name = "addExpenseBTN";
+            this.addExpenseBTN.Size = new System.Drawing.Size(75, 23);
+            this.addExpenseBTN.TabIndex = 2;
+            this.addExpenseBTN.Text = "Add";
+            this.addExpenseBTN.UseVisualStyleBackColor = true;
+            this.addExpenseBTN.Click += new System.EventHandler(this.addExpenseBTN_Click);
+            // 
+            // label5
+            // 
+            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(-1, -1);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(350, 30);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Expenses";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 33);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.Size = new System.Drawing.Size(342, 150);
+            this.dataGridView1.TabIndex = 7;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AllowUserToAddRows = false;
+            this.dataGridView2.AllowUserToDeleteRows = false;
+            this.dataGridView2.AllowUserToResizeColumns = false;
+            this.dataGridView2.AllowUserToResizeRows = false;
+            this.dataGridView2.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(3, 33);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.ReadOnly = true;
+            this.dataGridView2.RowHeadersVisible = false;
+            this.dataGridView2.Size = new System.Drawing.Size(342, 150);
+            this.dataGridView2.TabIndex = 8;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(184, 262);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(80, 20);
+            this.numericUpDown1.TabIndex = 8;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(184, 262);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(80, 20);
+            this.numericUpDown2.TabIndex = 9;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(746, 312);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "BusinessCalc";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button addIncomeBTN;
+        private System.Windows.Forms.Label ALL1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label ALL2;
+        private System.Windows.Forms.Button addExpenseBTN;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+    }
+}
+
---
+++ BusinessCalc/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SQLite;
+
+namespace BusinessCalc
+{
+    public partial class Form1 : Form
+    {
+        public static SQLiteConnection connection = new SQLiteConnection("Data Source=../../../database.db");
+        List<string[]> incomes;
+        List<string[]> expenses;
+        public Form1()
+        {
+            InitializeComponent();
+            UpdateAll();
+            dataGridView1.Columns[0].Name = "Theme";
+            dataGridView1.Columns[1].Name = "Count";
+            dataGridView1.Columns[2].Name = "Date";
+            dataGridView2.Columns[0].Name = "Theme";
+            dataGridView2.Columns[1].Name = "Count";
+            dataGridView2.Columns[2].Name = "Date";
+        }
+
+        private void addIncomeBTN_Click(object sender, EventArgs e)
+        {
+            connection.Open();
+            string request = $"INSERT INTO transactions (Theme, Count, Date) VALUES ('{comboBox1.Text}', {numericUpDown1.Value}, '{DateTime.Today.ToString("yyyy-MM-dd")}')";
+            var command = new SQLiteCommand(request, connection);
+            command.ExecuteNonQuery();
+            connection.Close();
+            UpdateAll();
+        }
+
+        private void addExpenseBTN_Click(object sender, EventArgs e)
+        {
+            connection.Open();
+            string request = $"INSERT INTO transactions (Theme, Count, Date) VALUES ('{comboBox2.Text}', {numericUpDown2.Value}, '{DateTime.Today.ToString("yyyy-MM-dd")}')";
+            var command = new SQLiteCommand(request, connection);
+            command.ExecuteNonQuery();
+            connection.Close();
+            UpdateAll();
+        }
+
+        public void UpdateAll()
+        {
+            incomes = getData("SELECT * FROM transactions WHERE Theme IN (SELECT Name FROM themes WHERE Type = 'Income')");
+            expenses = getData("SELECT * FROM transactions WHERE Theme IN (SELECT Name FROM themes WHERE Type = 'Expense')");
+            dataGridView1.ColumnCount = incomes[0].Length;
+            dataGridView1.RowCount = incomes.Count;
+            dataGridView2.ColumnCount = expenses[0].Length;
+            dataGridView2.RowCount = expenses.Count;
+
+            ALL1.Text = $"All: {getData("SELECT SUM(Count) FROM transactions WHERE Theme IN (SELECT Name FROM themes WHERE Type = 'Income')")[0][0]}$";
+            ALL2.Text = $"All: {getData("SELECT SUM(Count) FROM transactions WHERE Theme IN (SELECT Name FROM themes WHERE Type = 'Expense')")[0][0]}$";
+
+            for (var i = 0; i < incomes.Count; i++)
+            {
+                for (var j = 0; j < incomes[0].Length; j++)
+                {
+                    dataGridView1.Rows[i].Cells[j].Value = incomes[i][j];
+                }
+            }
+            for (var i = 0; i < expenses.Count; i++)
+            {
+                for (var j = 0; j < expenses[0].Length; j++)
+                {
+                    dataGridView2.Rows[i].Cells[j].Value = expenses[i][j];
+                }
+            }
+        }
+
+        public List<string[]> getData(string request)
+        {
+            connection.Open();
+
+            var command = new SQLiteCommand(String.Format(request), connection);
+            var tempList = new List<string>();
+            var result = new List<string[]>();
+
+            using (var reader = command.ExecuteReader())
+            {
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        tempList.Clear();
+                        for (var i = 0; i < reader.FieldCount; i++)
+                        {
+                            tempList.Add(Convert.ToString(reader.GetValue(i)));
+                        }
+                        var tempArray = new string[tempList.Count];
+                        tempList.CopyTo(tempArray);
+                        result.Add(tempArray);
+                    }
+                }
+            }
+            connection.Close();
+            return result;
+        }
+    }
+}
